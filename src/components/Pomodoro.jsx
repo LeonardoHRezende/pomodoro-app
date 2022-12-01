0ccf049b37e3667ba82b22b350fc6658e2d02a77
leonardo-assertech <leonardo-rezende@assertech.app>
2022-11-30 21:14:35 -0300

    Feat: Teste de adição de toque ao finalizar o timer

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,5 +1,8 @@
 import '../styles/main.css';
+
 import React, { useState, useEffect } from "react";
+
+import { Helmet } from "react-helmet";
 
 
 const Pomodoro = () => {
@@ -18,7 +21,23 @@ const Pomodoro = () => {
             }, 1000);
 
             if (time === 0) {
+
                 clearInterval(timer);
+
+                var context = new AudioContext();
+                var oscillator = context.createOscillator();
+                oscillator.type = "sine";
+                oscillator.frequency.value = 800;
+                oscillator.connect(context.destination);
+                oscillator.start();
+                // Beep for 500 milliseconds
+                setTimeout(function () {
+                    oscillator.stop();
+                }, 100);
+
+                setPaused(true)
+                setTime(1500)
+
             }
 
             return () => {
@@ -28,12 +47,12 @@ const Pomodoro = () => {
 
     });
 
-    function iniciaTimer(){
+    function iniciaTimer() {
 
-        if(!paused){
+        if (!paused) {
             setPaused(true)
         }
-        else{
+        else {
             setPaused(false)
         }
 
@@ -41,20 +60,19 @@ const Pomodoro = () => {
 
     return (
         <>
-            <h1 className="font-bold text-9xl text-white">
-                {minutes == 0 && seconds == 0 ?
-                    <span>
-                        25:00
-                    </span>
+            <Helmet>
+                <link rel="shortcut icon" href="https://icons.iconarchive.com/icons/elegantthemes/beautiful-flat-one-color/128/dev-icon.png" />
+                <title>{`${minutes == 0 ? `00` : minutes < 10 ? `0${minutes}` : minutes}:${seconds == 0 ? `00` : seconds < 10 ? `0${seconds}` : seconds} Hora de focar!`}</title>
+            </Helmet>
 
-                    :
-                    <span>{minutes}:{seconds == 0 ? `00` : seconds.length < 2 ? <span>0{seconds}</span> : seconds}</span>}
+            <h1 className="font-bold text-9xl text-white">
+                <span>{minutes == 0 ? `00` : minutes < 10 ? `0${minutes}` : minutes}:{seconds == 0 ? `00` : seconds < 10 ? `0${seconds}` : seconds}</span>
             </h1>
 
             <button
                 className="p-4 bg-violet-900 text-white font-bold rounded-md shadow-md text-4xl hover:bg-violet-700"
                 onClick={() => iniciaTimer()}>
-                Começar
+                {paused ? `Começar` : `Pausar`}
             </button>
         </>
     )

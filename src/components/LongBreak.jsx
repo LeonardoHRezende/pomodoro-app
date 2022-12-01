62f82fe49a59c08fb6499ea9569455a562c633ad
leonardo-assertech <leonardo-rezende@assertech.app>
2022-11-30 21:14:56 -0300

    Feat: Adição da função de pausa

--- src/components/LongBreak.jsx
+++ src/components/LongBreak.jsx
@@ -1,41 +1,60 @@
 import '../styles/main.css';
+
 import React, { useState, useEffect } from "react";
+
+import { Helmet } from 'react-helmet';
 
 
 const LongBreak = () => {
-    const [time, setTime] = useState(0);
+    const [time, setTime] = useState(600);
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
+    const [paused, setPaused] = useState(true)
+
     useEffect(() => {
 
-        const timer = setInterval(() => {
-            setTime(time - 1);
-        }, 1000);
+        if (!paused) {
+            const timer = setInterval(() => {
+                setTime(time - 1);
+            }, 1000);
 
-        if (time === 0) {
-            clearInterval(timer);
+            if (time === 0) {
+                clearInterval(timer);
+            }
+
+            return () => {
+                clearInterval(timer);
+            };
         }
 
-        return () => {
-            clearInterval(timer);
-        };
     });
+
+    function iniciaTimer() {
+
+        if (!paused) {
+            setPaused(true)
+        }
+        else {
+            setPaused(false)
+        }
+
+    }
+
     return (
         <>
-            <h1 className="font-bold text-9xl text-white">
-                {minutes == 0 && seconds == 0 ?
-                    <span>
-                        10:00
-                    </span>
+            <Helmet>
+                <link rel="shortcut icon" href="https://icons.iconarchive.com/icons/elegantthemes/beautiful-flat-one-color/128/dev-icon.png" />
+                <title>{`${minutes == 0 ? `00` : minutes < 10 ? `0${minutes}` : minutes}:${seconds == 0 ? `00` : seconds < 10 ? `0${seconds}` : seconds} Hora da pausa!`}</title>
+            </Helmet>
 
-                    :
-                    <span>{minutes}:{seconds == 0 ? `00` : seconds.length < 2 ? <span>0{seconds}</span> : seconds}</span>}
+            <h1 className="font-bold text-9xl text-white">
+                <span>{minutes == 0 ? `00` : minutes < 10 ? `0${minutes}` : minutes}:{seconds == 0 ? `00` : seconds < 10 ? `0${seconds}` : seconds}</span>
             </h1>
 
             <button
                 className="p-4 bg-violet-900 text-white font-bold rounded-md shadow-md text-4xl hover:bg-violet-700"
-                onClick={() => setTime(600)}>
-                Começar
+                onClick={() => iniciaTimer()}>
+                {paused ? `Começar` : `Pausar`}
             </button>
         </>
     )

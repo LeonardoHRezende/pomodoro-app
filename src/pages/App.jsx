63d07e134919e3c03819806c246649d7809ef14f
leonardo-assertech <leonardo-rezende@assertech.app>
2022-11-30 21:13:42 -0300

    Feat: Adição de nome do app e icon

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,12 @@
 import '../styles/main.css';
+
 import React, { useState } from "react";
+
 import Pomodoro from '../components/Pomodoro';
 import ShortBreak from '../components/ShortBreak';
 import LongBreak from '../components/LongBreak';
 
+import { BsCodeSlash } from "react-icons/bs";
 
 
 const App = () => {
@@ -13,9 +16,14 @@ const App = () => {
 
   return (
     <>
-      <div className="max-w-[1344px] min-h-screen mx-auto items-center flex flex-col justify-center gap-5">
+      <div className="max-w-[1344px] min-h-screen mx-auto items-center flex flex-col gap-5">
 
-        <div className="flex flex-col gap-6 items-center rounded-lg shadow-md bg-[#7a55ce] bg-opacity-60  w-[900px] h-[500px]">
+        <div className='mt-10 flex flex-row gap-3'>
+          <BsCodeSlash className="font-semibold text-gray-200" size={30} />
+          <h1 className='text-2xl font-semibold text-gray-200'>PomodoroApp</h1>
+        </div>
+
+        <div className="flex flex-col gap-20 items-center rounded-lg shadow-md bg-[#7a55ce] bg-opacity-60  w-[900px] h-[500px]">
 
           <div className="flex flex-row gap-6 justify-between items-stretch rounded-lg shadow-md bg-[#8a6dcc] w-3/4 py-3 px-2 my-5">
 
@@ -43,7 +51,7 @@ const App = () => {
 
       </div>
     </>
-  ); m
+  );
 };
 
 export default App
